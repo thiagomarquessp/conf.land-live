16b503ffcb7815684f414fcc5a6ea4b8273758d4
Thiago Pereira <thiagomarquessp@gmail.com>
2018-11-23 18:45:14 +0000

    Adjustment acceptance criteria.

--- tests/e2e/validations/validations.js
+++ tests/e2e/validations/validations.js
@@ -4,22 +4,25 @@ global.LoginValidations = require('./validationsPage');
 describe('Login form validations', function() {
   beforeAll(function() { 
     browser.waitForAngularEnabled(false);
+    LoginValidations.get();
   });
 
-  it('Validate the login form size', function() {
-    LoginValidations.get();
-    expect(size_username.getAttribute('size')).toEqual('20');
+  it('The username field should have a size of 20 characters', function() {
     expect(size_password.getAttribute('size')).toEqual('20');
   });
 
-  it('Validate username error message to login', function() {
+  it('The password field should have a size of 20 characters', function() {
+    expect(size_password.getAttribute('size')).toEqual('20');
+  });
+
+  it('Error message when the user put invalid username', function() {
     LoginValidations.setName('erroruser');
     LoginValidations.setPass('errorpwd');
     LoginValidations.btnLogin();
     browser.wait(alertPresent.textToBePresentInElement($('.response'), 'ERROR: Invalid username. '), 5000);
   });
 
-  it('Validate password error message to login', function() {
+  it('error message when the user put an invalid password', function() {
     element(by.name('log')).clear();
     LoginValidations.setName('test');
     LoginValidations.btnLogin();
